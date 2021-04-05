8b395628491450f0c0cd87d8da45c20cc8b7a002
GHOST #IU <74893002+Ghost-IU@users.noreply.github.com>
2021-04-05 10:14:25 +0530

    Added Initial Code

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+import os
+import sys
+from PIL import Image
 
+input_folder = sys.argv[1]
+output_folder = sys.argv[2]
+
+if not os.path.exists(output_folder):
+  os.makedirs(output_folder)
+
+count = 0
+for image in os.listdir(input_folder):
+  img = Image.open(f'{input_folder}{image}')
+  new_name = os.path.splitext(image)[0]
+  img.save(f'{output_folder}{new_name}.png' , 'png')
+  count = count + 1
+  print(f'Converting image {count} .....')
+  
+print('Task Compplete')
