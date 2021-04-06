e031f7b83010e7818293e4b4fb3d784f7f6982d6
GHOST #IU <74893002+Ghost-IU@users.noreply.github.com>
2021-04-06 22:32:40 +0530

    Update script.py

--- script.py
+++ script.py
@@ -2,18 +2,18 @@ import os
 import sys
 from PIL import Image
 
-input_folder = sys.argv[1]
-output_folder = sys.argv[2]
+input_folder = sys.argv[1]                           #Grab the first argument
+output_folder = sys.argv[2]                          #Grab the second argument
 
-if not os.path.exists(output_folder):
+if not os.path.exists(output_folder):                #Check if the output folder exists 
   os.makedirs(output_folder)
 
 count = 0
-for image in os.listdir(input_folder):
+for image in os.listdir(input_folder):                #Loop through the input folder
   img = Image.open(f'{input_folder}{image}')
   new_name = os.path.splitext(image)[0]
-  img.save(f'{output_folder}{new_name}.png' , 'png')
+  img.save(f'{output_folder}{new_name}.png' , 'png')  #Convert them to PNG and save it to new folder
   count = count + 1
-  print(f'Converting image {count} .....')
+  print(f'Converting image {count} .....')            
   
 print('Task Complete')
